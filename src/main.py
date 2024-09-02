1e5d464f217ab381f43135b048e2b3a2a7000073
unknown <alohakii@naver.com>
2024-09-03 00:13:57 +0900

    update

--- src/main.py
+++ src/main.py
@@ -1,17 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
+from web import explorer, creature
 
 app = FastAPI()
-
-@app.get("/")
-def top():
-    return "top here"
-
-@app.get("/echo/{thing}")
-def echo(thing:str):
-    return f"echoing {thing}"
+app.include_router(explorer.router)
+app.include_router(creature.router)
 
 if __name__ == "__main__":
-    print("uvicorn 으로 실행합니다.")
-    import uvicorn
-    uvicorn.run("main:app", reload = True)
-
+    uvicorn.run("main:app", reload=True)
